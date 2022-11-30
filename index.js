9ea86aaeaa35443cf8bc93ff61189879010f46ec
0xlAp404# <afdipratama07@gmail.com>
2022-11-30 17:23:26 +0700

    Updating Hint Answer With Result Account.txt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer-extra');
 const fetch = require('node-fetch');
+const ncp = require("copy-paste");
 const chromePaths = require('chrome-paths');
 const cheerio = require('cheerio');
 const fs = require('fs');
@@ -78,7 +79,7 @@ const lastInfo = randlast();
     await page.bringToFront()
     
 
-    await sleep (2000, console.log("=> Mencoba load URL..."))
+    await sleep (2000, console.log("==> Mencoba load URL : https://music.apple.com/"))
 
     await page.setViewport({ width: 1280, height: 720})
 
@@ -107,7 +108,6 @@ const lastInfo = randlast();
     await page.waitForSelector('#acAccountPassword')
     await page.click('#acAccountPassword')
     await page.keyboard.type(password)
-    await sleep(1000, console.log("=> Membuat akun dengan Username : " + emailInfo + nomor + "@bheps.com|" + password))
     await page.waitForSelector('#agreedToTerms')
     await page.click('#agreedToTerms')
     await sleep(1000)
@@ -115,8 +115,21 @@ const lastInfo = randlast();
     await page.click('.account-base__content > .create-account > .create-account__form > .form-buttons > .web-button-form')
     await sleep(1000)
     await sleep(1000)
+    await sleep(1000)
+    await sleep(1000)
     await pageTwo.bringToFront()
-    await sleep(9000)
+    const appInfo = await page.waitForSelector('span[class="verify-email-sublabel__email"');
+    const value = await appInfo.evaluate(el => el.textContent);
+    
+
+    // And here is your game names
+    
+    await sleep(1000, console.log("==> Membuat akun Apple : " + value + "|" + password))       
+    await sleep(6000)
+    fs.appendFile("akun.txt", value + "|" + password + "\n" , function (err) {
+
+        if (err) return console.log(err);
+      });
     await pageTwo.evaluate(() => {
         location.reload(true)
      })
@@ -131,12 +144,18 @@ const lastInfo = randlast();
     
     
     await sleep(3000)   
+
+
+    
+
     const elementHandle = await pageTwo.$('iframe[class="bodyHTML"');
     const frame = await elementHandle.contentFrame();
+    const getOtp = await frame.$('#main > div.container > div > div > p > b');
+    const Otp1 = await getOtp.evaluate(el => el.textContent);
 
     const click = await frame.$('#main > div.container > div > div > p > b');
     await click.click({clickCount:3})
-    await sleep(1000)
+    await sleep(1000,console.log('==> OTP Apple : ' + Otp1))
     await pageTwo.keyboard.down('ControlLeft')
     await pageTwo.keyboard.press('C')
     await pageTwo.keyboard.up('ControlLeft')
@@ -146,10 +165,10 @@ const lastInfo = randlast();
     await page.keyboard.down('ControlLeft')
     await page.keyboard.press('V')
     await page.keyboard.up('ControlLeft')
-
+                      
                     await page.waitForSelector('#app > div > div > div > main > div.account-base__content > div > form > div.form-buttons > button.web-button-form.form-button.web-button-form--primary.form-buttons__continue.form-buttons__button')
                     await page.click('#app > div > div > div > main > div.account-base__content > div > form > div.form-buttons > button.web-button-form.form-button.web-button-form--primary.form-buttons__continue.form-buttons__button')
-                    await sleep(2000, console.log("=> Mengisi data field address & billing info... "))
+                    await sleep(2000, console.log("==> Mengisi data field address & billing info... "))
                     await page.waitForSelector('#creditCardNumber')
                     await page.click('#creditCardNumber')
 
@@ -228,6 +247,7 @@ const lastInfo = randlast();
                     await sleep(4000)
                     await sleep(4000)
                     await sleep(4000)
+                    await sleep(4000)
                     await sleep(4000) 
                     await page.keyboard.press("Tab");
                     await page.keyboard.press("Tab");
@@ -235,31 +255,50 @@ const lastInfo = randlast();
                     await page.keyboard.press("Tab");
                     await page.keyboard.press("Space");
                     await sleep(5000)
-                    await sleep(5000)
                     await page.click('span[class="web-button__text"')
+                    await sleep(5000 , console.log("==> Berhasil Membuat Akun Apple.."))
                     await sleep(1000)
                     await page.goto('https://appleid.apple.com/sign-in')
-                    await sleep (2000)
+                    await console.log('==> Mencoba Membuat Hint Verification')
                     await pageTwo.click('.mail-head > .nop > li > .copytrg > .icon-copy')
-                    await sleep(2000)
+                    
+                    await page.evaluate(() => {
+                        location.reload(true)
+                     })
+                     await sleep(6000)
+                     await sleep (4000)
                     const appleUname = await page.$('iframe[name="aid-auth-widget"');
                     const getFrame = await appleUname.contentFrame();
 
                     const field1 = await getFrame.$('#account_name_text_field');
                     const field2 = await getFrame.$('#password_text_field');
                     const button = await getFrame.$('#sign-in');
+                    await sleep(4000)
                     await field1.click({clickCount:2});
                     await page.keyboard.down('ControlLeft')
                     await page.keyboard.press('V')
                     await page.keyboard.up('ControlLeft')
-                    await button.click({clickCount:3});
-                    await sleep(4000)
-                    await field2.type(password)
-                    await sleep(3000)
+                    await sleep(1000)
                     await button.click({clickCount:3});
                     await sleep(5000)
-                    await pageTwo.waitForSelector('.os-padding > .os-viewport > .os-content > .inbox-list > .inbox-one')
-                    await pageTwo.click('.os-padding > .os-viewport > .os-content > .inbox-list > .inbox-one')
+                    await field2.type(password)
+                    await sleep(4000)
+                    await button.click({clickCount:3});
+                    await sleep(5000)
+                    await pageTwo.evaluate(() => {
+                        location.reload(true)
+                     })
+                    await sleep(1000)
+                    await console.log('==> Verifikasi OTP...')
+                    await pageTwo.click('.mailbox > .mail-container > .message-inbox > .cta-refresh > a')
+                    await pageTwo.waitForSelector('.os-padding > .os-viewport > .os-content > .inbox-list > .inbox-one:nth-child(1)')
+                    await pageTwo.click('.os-padding > .os-viewport > .os-content > .inbox-list > .inbox-one:nth-child(1)')
+                    await sleep(3000)   
+                    const elementHandle2 = await pageTwo.$('iframe[class="bodyHTML"');
+                    const frame2 = await elementHandle2.contentFrame();
+                
+                    const click2 = await frame2.$('#main > div.container > div > div > b > p');
+                    await click2.click({clickCount:3})
                     await pageTwo.keyboard.down('ControlLeft')
                     await pageTwo.keyboard.press('C')
                     await pageTwo.keyboard.up('ControlLeft')
@@ -267,8 +306,59 @@ const lastInfo = randlast();
                     await page.keyboard.down('ControlLeft')
                     await page.keyboard.press('V')
                     await page.keyboard.up('ControlLeft')
-		     
-		    // EMAIL SIGN SAMPLE
+                    await sleep(19000)
+                    const appleUname3 = await page.$('iframe[id="repairFrame"');
+                    const getFrame3 = await appleUname3.contentFrame();
+                    const field3 = await getFrame3.$('input[class="form-textbox form-textbox-text"');
+                    const button2 = await getFrame3.$('button[type="submit"')
+                    // let anchorElem1 = await field3.$("a[class='email']");
+                    let link1 = field3.attr("id"); 
+                    await link1.click()
+                    await sleep(1000)
+                    await link1.type(vcc)
+                    await sleep(1000)
+                    await button2.click({clickCount: 2})
+                    await sleep(10000)
+                    // HINT 1
+                    const hint1 = await page.$('iframe[id="repairFrame"');
+                    const getHint1 = await hint1.contentFrame();
+                    const fieldHint1 = await getHint1.$('select[class="form-dropdown"');
+                    await fieldHint1.click();
+                    await sleep(1000)
+                    await fieldHint1.press('ArrowDown');
+                    await fieldHint1.press('ArrowDown');
+                    await fieldHint1.press('Enter');
+                    const fieldHint2 = await getHint1.$('input[class="form-textbox"')
+                    await fieldHint2.click({clickCount:2})
+                    await fieldHint2.type('Mory')
+                    const buttonHint1 = await getHint1.$('button[type="submit"')
+                    await buttonHint1.click({clickCount:2})
+                    // HINT 2
+                    await sleep(2000)
+                    const fieldHint3 = await getHint1.$('select[class="form-dropdown"')
+                    await fieldHint3.press('ArrowDown');
+                    await fieldHint3.press('ArrowDown');
+                    await fieldHint3.press('Enter');
+                    const fieldHint4 = await getHint1.$('input[class="form-textbox"')
+                    await fieldHint4.click({clickCount:2})
+                    await fieldHint4.type('Tidur')
+                    const buttonHint2 = await getHint1.$('button[type="submit"')
+                    await buttonHint2.click({clickCount:2})
+                    // HINT 3
+                    
+                    await sleep(2000)
+                    const fieldHint5 = await getHint1.$('select[class="form-dropdown"')
+                    await fieldHint5.press('ArrowDown');
+                    await fieldHint5.press('ArrowDown');
+                    await fieldHint5.press('Enter');
+                    const fieldHint6 = await getHint1.$('input[class="form-textbox"')
+                    await fieldHint6.click({clickCount:2})
+                    await fieldHint6.type('Monyong')
+                    const buttonHint3 = await getHint1.$('button[type="submit"')
+                    await buttonHint3.click({clickCount:2})
+                    await sleep(5000)
+
+                    // donny.lockman77@delivrmail.com
                     // walton_wiegand6@delivrmail.com
                     // Anjing404#!
             })();
